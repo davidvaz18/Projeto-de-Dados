31cb1f72643fa5ae01c3386e1c9d059960d1d829
David Vaz <vazdavid460@gmail.com>
2026-04-16 13:26:03 -0300

    Implementacao local do Kakfka com Docker

---
+++ src/teste_conexao.py
@@ -0,0 +1,10 @@
+from kafka import KafkaProducer
+
+print("⏳ Tentando conectar ao Kafka no Docker...")
+
+try:
+    producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092')
+    print("✅ SUCESSO! O Python conseguiu falar com o Kafka.")
+    producer.close()
+except Exception as e:
+    print(f"❌ ERRO: Não conectou. Motivo: {e}")
